27747cca5311121f97635fe31a26e166f70d2f71
Richard E. Dodson <richard.elias.dodson@gmail.com>
2015-11-05 16:40:07 -0500

    Initialize src/term-put-underline.c

---
+++ src/term-put-underline.c
@@ -0,0 +1,32 @@
+//  
+//  Name:     rdodson41/term-put/src/term-put-underline.c
+//  Author:   Richard E. Dodson <richard.elias.dodson@gmail.com>
+//  Created:  Thu Nov  5 21:16:18 UTC 2015
+//  License:  GNU General Public License, Version 3, 29 June 2007
+//  
+//  Copyright (C) 2015 Richard E. Dodson <richard.elias.dodson@gmail.com>
+//  
+//  This file is part of term-put.
+//  
+//  term-put is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  term-put is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with term-put. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <stdio.h>
+
+static const char UNDERLINE[] = "\x1b[4m";
+
+//  Enable underlined terminal output
+int term_put_underline() {
+	return fwrite(UNDERLINE, sizeof(char), sizeof(UNDERLINE) - sizeof(char), stdout);
+}
